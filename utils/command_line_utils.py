e7b1423a862fc095f27fec56d2baae1c0a51bfe7
cmartin <cmartin@ebi.ac.uk>
2023-11-23 18:21:13 +0000

    command line utils comments

--- utils/command_line_utils.py
+++ utils/command_line_utils.py
@@ -2,8 +2,17 @@ from argparse import Namespace
 
 
 class CommandLineUtils:
+    """
+    Collection of static command line related methods.
+    """
+
     @staticmethod
     def print_line_of_token(token: str = "_"):
+        """
+        Print a line to the console with the given token, default is '_'
+        :param token: Token of which the printed line will consist.
+        :return: N/A prints line of tokens.
+        """
         line_length = 101  # Length of the given line
         token_length = len(token)
         repeat_count = (
@@ -16,6 +25,12 @@ class CommandLineUtils:
 
     @staticmethod
     def readout(*args):
+        """
+        Print out the keys and values from all the arguments. This is a 'stupid' method, and assumes you have provided
+        it with dicts or objects that can be turned into dicts. It will fail, as it should, if you don't.
+        :param args: Iterable objects or dict objects.
+        :return: N/A prints contents of args
+        """
         CommandLineUtils.print_line_of_token("#")
         print("all config values and command line arguments:")
         for arg in args:
